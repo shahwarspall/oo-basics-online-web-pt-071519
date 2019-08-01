a617f95ba714145482e0f1a71a95187aa4e8e222
Shahwar Spall <shevy.spall@gmail.com>
2019-08-01 03:51:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,11 +5,11 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+  def brand 
+  @brand
+end
   def cobble
     puts "Your shoe is as good as new!"
-end
-def brand 
-  @brand
 end
 
 end
